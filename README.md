d494b8fc2171cf8cd0bcbff567e526838f4e6eee
jp <janis.puris@gmail.com>
2020-11-17 19:57:22 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # macos-vpn-traffic-dns-split
 Traffic and DNS split tunnelling for MacOS when using VPN
+
+## Overview
+TODO: more info
+
+## dnsmasq setup
+
+### Install
+
+```sh
+brew install dnsmasq
+```
+
+### Configure
+
+```sh
+cp dnsmasq.conf /usr/local/etc/dnsmasq.conf
+```
+
+## Network override setup
+
+```sh
+mkdir -p $HOME/.util/scripts
+cp scripts/network-override.sh $HOME/.util/scripts/
+```
+
+```sh
+cp agent/network-override.plist /Library/LaunchDaemons/
+sudo launchctl load -w /Library/LaunchDaemons/network.watcher.plist
+```
+
+## Useful commands
+
+### Check routes
+
+```sh
+watch -d "netstat -rn"
+```
+
+### Check for DNS leaks
+
+```sh
+sudo tcpdump -s0 -lvni utun2 'udp port 53'
+sudo tcpdump -s0 -lvni lo0 'udp port 53'
+sudo tcpdump -s0 -lvni en0 'udp port 53'
+```
---
+++ scripts/network-override.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# SETTINGS
+dns="127.0.0.1"
+gateway="A.A.A.A"
+vpn_networks="B.B.B.B/24 C.C.C.C/24 D.D.D.D/16"
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Run this as root"
+    exit 1
+fi
+
+# If your utun interface tend to change like mine you can do the following given the right interface is the last one in the list
+INTERFACE=$(ifconfig | grep utun | cut -d : -f 1 | grep utun | tail -n 1)
+
+# last line in the file is the gateway ip
+route change default ${gateway}
+for net in ${vpn_networks}; do route add -net ${net} -interface $INTERFACE; done;
+
+# Update DNS in resolv.conf
+cat <<EOF | sudo tee /etc/resolv.conf
+nameserver ${dns}
+EOF
+
+# Update DNS in system db
+states=$(scutil <<< list | awk '{ print $4 }' | egrep '^State:/Network/Service/[^/]+/DNS$')
+while read state; do printf "d.init\nget ${state}\nd.add ServerAddresses * ${dns}\nset ${state}\nquit\n" | sudo scutil; done <<<"$states"
+
+# Flush
+sudo killall -HUP mDNSResponder
+sudo killall mDNSResponderHelper
+sudo dscacheutil -flushcache
