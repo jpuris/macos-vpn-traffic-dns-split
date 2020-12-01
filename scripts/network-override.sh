a6da68b1e85ac45d7b8ddf47fe8a0470acef3ded
jp <janis.puris@gmail.com>
2020-12-01 16:56:54 +0100

    slight refactor

--- scripts/network-override.sh
+++ scripts/network-override.sh
@@ -2,6 +2,8 @@
 
 # SETTINGS
 dns="127.0.0.1"
+DNS_RESOLVE="127.0.0.1"
+DNS="1.1.1.1 1.0.0.1"
 gateway="A.A.A.A" # TODO: detect automatically
 vpn_networks="B.B.B.0/24 C.C.C.0/24 D.D.0.0/16"
 
@@ -10,21 +12,23 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# VPN Traffic split
 # If your utun interface tend to change like mine you can do the following given the right interface is the last one in the list
 INTERFACE=$(ifconfig | grep -A1 utun | grep -B1 'inet ' | grep utun | cut -d : -f 1 | grep utun | tail -n 1)
 
 # last line in the file is the gateway ip
 route change default ${gateway}
-for net in ${vpn_networks}; do route add -net ${net} -interface $INTERFACE; done;
+for NET in ${NETWORKS}; do route add -net ${NET} -interface $INTERFACE; done;
 
+# DNS
 # Update DNS in resolv.conf
 cat <<EOF | sudo tee /etc/resolv.conf
-nameserver ${dns}
+nameserver ${DNS_RESOLVE}
 EOF
 
 # Update DNS in system db
-states=$(scutil <<< list | awk '{ print $4 }' | egrep '^State:/Network/Service/[^/]+/DNS$')
-while read state; do printf "d.init\nget ${state}\nd.add ServerAddresses * ${dns}\nset ${state}\nquit\n" | sudo scutil; done <<<"$states"
+STATES=$(scutil <<< list | awk '{ print $4 }' | egrep '^State:/Network/Service/[^/]+/DNS$' | grep -v 'gpd.pan')
+while read STATE; do printf "d.init\nget ${STATE}\nd.add ServerAddresses * ${DNS}\nset ${STATE}\nquit\n" | sudo scutil; done <<<"$STATES"
 
 # Flush
 sudo killall -HUP mDNSResponder
